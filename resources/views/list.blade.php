e9ea6d9343d80816517de9ca35a6f1da167b126b
bencotti21 <bencotti21@gmail.com>
2023-06-13 22:37:39 +0900

    add list view

---
+++ resources/views/list.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ぺっとコメンツ</title>
+</head>
+<body>
+    <p>ユーザー名・<?= date("Y年n月j日") ?></p>
+    <p>コメント</p>
+</body>
+</html>
