b322d4cf5b9ee50ea196db73a65e8de74d816a29
bencotti21 <bencotti21@gmail.com>
2023-06-19 17:42:44 +0900

    update list view

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -2,22 +2,48 @@
 
 @section('content')
   @auth
-  <form method="POST" action="{{ route('comment.store') }}">
-    @csrf
-    <p>コメント</p>
-    <textarea name="comment"></textarea>
-    <input type="submit" value="コメントする">
-  </form>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-6 mb-4">
+        <form method="POST" action="{{ route('comment.store') }}">
+          @csrf
+          <div class="form-group mb-2">
+            <textarea name="comment" style="width:100%;" class="form-control"></textarea>
+          </div>
+          <div style="text-align:right;">
+            <input type="submit" value="コメントする">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
   @endauth
 
   @foreach ($comments as $comment)
-    <p>{{ $comment->user->name }}・{{ \Common::getDiffTime($comment) }}
-      {{ \Common::getUpdatedWord($comment) }}
-    </p>
-    <p>
-      <a href="{{ route('comment.show', ['id' => $comment->id]) }}">
-        {{ $comment->comment }}
-      </a>
-    </p>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card mb-2">
+            <a href="{{ route('comment.show', ['id' => $comment->id]) }}" style="text-decoration:none;color:black;">
+              <div class="media">
+                <div class="media-body">
+                  <div style="padding:8px;">
+                    <span><strong>{{ $comment->user->name }}</strong></span>
+                    <span class="text-muted">
+                      <span>・</span>
+                      <span>{{ \Common::getDiffTime($comment) }}</span>
+                      <span>{{ \Common::getUpdatedWord($comment) }}</span>
+                    </span>
+                  </div>
+                  <div style="padding:8px 16px;">
+                      <span>{{ $comment->comment }}</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   @endforeach
 @endsection
