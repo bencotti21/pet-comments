62de939e40d31322cbe101cca8e8bdc237c7ea61
bencotti21 <bencotti21@gmail.com>
2023-06-23 15:26:30 +0900

    fix reset password
    ボタンを押下するとメッセージが表示されるように修正

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
+                    {{-- <form method="POST" action="{{ route('password.email') }}"> --}}
+                    <form method="POST" action="#">
                         @csrf
 
                         <div class="row mb-3">
@@ -33,14 +34,18 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                {{-- <button type="submit" class="btn btn-primary">
                                     {{ __('Send Password Reset Link') }}
-                                </button>
+                                </button> --}}
+                                <input type="button" class="btn btn-primary" value="パスワード再設定ＵＲＬを送信" 
+                                    onclick="disabled=true;
+                                            document.getElementById('reset-message').innerHTML='<div class=\'alert alert-success\'>メール機能は未実装のため、パスワードの再設定はご利用いただけません。</div>';">
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+            <div style="margin-top:16px" id="reset-message"></div>
         </div>
     </div>
 </div>
